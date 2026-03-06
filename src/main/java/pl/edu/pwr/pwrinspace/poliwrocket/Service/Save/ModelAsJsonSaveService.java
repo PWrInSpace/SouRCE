cdb9f8429ad77485be222a9687691518961b7512
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-03-06 20:59:20 +0100

    Changing config format to yaml: Changing serialization library from Gson to Jackson: - removed ModelAsJsonSaveService.java and RuntimeTypeAdapterFactory.java and created ModelAsYamlService.java

--- src/main/java/pl/edu/pwr/pwrinspace/poliwrocket/Service/Save/ModelAsJsonSaveService.java
+++ src/main/java/pl/edu/pwr/pwrinspace/poliwrocket/Service/Save/ModelAsJsonSaveService.java
@@ -1,169 +1,172 @@
-package pl.edu.pwr.pwrinspace.poliwrocket.Service.Save;
-
-import com.google.gson.*;
-import com.google.gson.stream.JsonReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import pl.edu.pwr.pwrinspace.poliwrocket.Model.BaseSaveModel;
-import pl.edu.pwr.pwrinspace.poliwrocket.Model.Command.*;
-import pl.edu.pwr.pwrinspace.poliwrocket.Model.Sensor.*;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Iterator;
-import java.util.List;
-
-public class ModelAsJsonSaveService {
-
-    private static final Logger logger = LoggerFactory.getLogger(ModelAsJsonSaveService.class);
-    private final RuntimeTypeAdapterFactory<Sensor> sensorAdapterFactory = RuntimeTypeAdapterFactory.of(Sensor.class, "type")
-            .registerSubtype(Sensor.class, "Sensor")
-            .registerSubtype(FillingLevelSensor.class, "FillingLevelSensor")
-            .registerSubtype(TareSensor.class, "TareSensor")
-            .registerSubtype(AlertSensor.class, "AlertSensor")
-            .registerSubtype(TanwiarzSensor.class, "TanwiarzSensor")
-            .registerSubtype(PoteznyTanwiarzSensor.class, "PoteznyTanwiarzSensor")
-            .registerSubtype(SettingsSensor.class, "SettingsSensor")
-            .registerSubtype(CompositeBitSensor.class, "CompositeBitSensor")
-            .registerSubtype(ByteSensor.class, "ByteSensor")
-            .registerSubtype(TimerSensor.class, "TimerSensor");
-
-    private final RuntimeTypeAdapterFactory<Command> commandAdapterFactory = RuntimeTypeAdapterFactory.of(Command.class, "type")
-            .registerSubtype(ProtobufCommand.class, "ProtobufCommand")
-            .registerSubtype(ProtobufSimpleCommand.class, "ProtobufSimpleCommand")
-            .registerSubtype(StandardCommand.class, "StandardCommand");
-
-    // temp:
-    // true - zapisuje konfiguracje z do temp
-    // false - zapisuje konfiguracje do config
-    public void saveToFile(BaseSaveModel configuration, boolean temp) {
-        String configContent = new Gson().newBuilder()
-                .registerTypeAdapterFactory(sensorAdapterFactory)
-                .registerTypeAdapterFactory(commandAdapterFactory)
-                .excludeFieldsWithoutExposeAnnotation()
-                .setPrettyPrinting()
-                .disableHtmlEscaping()
-                .create()
-                .toJson(configuration);
-
-        Path configPath = Paths.get(configuration.getPath() + configuration.getFileName());
-        if (temp) configPath = Paths.get(configuration.getPath(), configuration.getTempFileName());
-
-        File configFile = new File(configPath.toString());
-
-        try (FileWriter configWriter = new FileWriter(configFile)) {
-            configWriter.write(configContent);
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void persistOldFile(BaseSaveModel config) {
-        File dir = new File(config.getPath());
-        if(!dir.exists()) {
-            dir.mkdir();
-        }
-
-        File configFile = new File(config.getPath() + config.getFileName());
-        File copy = new File(config.getPath()  + config.getPersistPrefix() + config.getFileName());
-        try (FileInputStream fis = new FileInputStream(configFile);
-           FileOutputStream fos = new FileOutputStream(copy)) {
-            int len;
-            byte[] buffer = new byte[4096];
-            while ((len = fis.read(buffer)) > 0) {
-                fos.write(buffer, 0, len);
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    // temp:
-    // true — czyta plik z temp
-    // false — ładuje plik config, a następnie nadpisuje nim temp i czyta plik temp
-    public <T extends BaseSaveModel> T readFromFile(T config, boolean temp) throws Exception {
-        if (!temp) {
-            Path configPath = Paths.get(config.getPath() + config.getFileName());
-            Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
-            Files.copy(configPath, configTempPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-
-        File configFile = new File(config.getPath() + config.getTempFileName());
-
-        try (JsonReader reader = new JsonReader(new FileReader(configFile))) {
-            config = new Gson().newBuilder()
-                    .registerTypeAdapterFactory(sensorAdapterFactory)
-                    .registerTypeAdapterFactory(commandAdapterFactory)
-                    .excludeFieldsWithoutExposeAnnotation().create().fromJson(reader, config.getClass());
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-            throw new Exception(e.getMessage());
-        }
-
-        return config;
-    }
-
-    public <T extends BaseSaveModel> void removeTempConfig(T config) {
-        File configTempFile = new File(config.getPath() + config.getTempFileName());
-        if (configTempFile.delete()) System.out.println("Removed temp file");
-        else System.out.println("Temp file not found");
-    }
-
-    public String commandToJson(Command command) {
-        return new Gson().newBuilder()
-                .registerTypeAdapterFactory(commandAdapterFactory)
-                .excludeFieldsWithoutExposeAnnotation()
-                .setPrettyPrinting()
-                .disableHtmlEscaping()
-                .create()
-                .toJson(command, Command.class);
-    }
-
-    public void addCommandToFile(BaseSaveModel config, Command command) {
-        String[] commandJson = commandToJson(command).split("\n");
-
-        Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
-
-        try {
-            List<String> configLines = Files.readAllLines(configTempPath);
-            int index = 0;
-            int squareBracketCounter = 1;
-            while(!configLines.get(index).contains("commandsList")) index++;
-            while(squareBracketCounter != 0) {
-                index++;
-                if(configLines.get(index).contains("[")) squareBracketCounter++;
-                if(configLines.get(index).contains("]")) squareBracketCounter--;
-            }
-            configLines.set(index-1, configLines.get(index-1) + ",");
-            for (String line : commandJson) {
-                configLines.add(index, "\t"+line);
-                index++;
-            }
-
-            Files.write(configTempPath, configLines);
-
-        } catch (FileNotFoundException e) {
-            logger.error("Config file not found");
-            logger.error(e.getMessage());
-        } catch (IOException e) {
-            logger.error("Error during modifying tempConfig");
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void addDestinationToCommand(BaseSaveModel config, Command command, String controllerName) {
-    }
-
-    public void overrideConfig(BaseSaveModel config) {
-        Path configPath = Paths.get(config.getPath() + config.getFileName());
-        Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
-        try {
-            Files.copy(configTempPath, configPath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-            System.out.println(e.getMessage());
-        }
-    }
-}
+//package pl.edu.pwr.pwrinspace.poliwrocket.Service.Save;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.stream.JsonReader;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import pl.edu.pwr.pwrinspace.poliwrocket.Model.BaseSaveModel;
+//import pl.edu.pwr.pwrinspace.poliwrocket.Model.Command.Command;
+//import pl.edu.pwr.pwrinspace.poliwrocket.Model.Command.ProtobufCommand;
+//import pl.edu.pwr.pwrinspace.poliwrocket.Model.Command.ProtobufSimpleCommand;
+//import pl.edu.pwr.pwrinspace.poliwrocket.Model.Command.StandardCommand;
+//import pl.edu.pwr.pwrinspace.poliwrocket.Model.Sensor.*;
+//
+//import java.io.*;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardCopyOption;
+//import java.util.List;
+//
+//public class ModelAsJsonSaveService {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(ModelAsJsonSaveService.class);
+//    private final RuntimeTypeAdapterFactory<Sensor> sensorAdapterFactory = RuntimeTypeAdapterFactory.of(Sensor.class, "type")
+//            .registerSubtype(Sensor.class, "Sensor") // done
+//            .registerSubtype(FillingLevelSensor.class, "FillingLevelSensor") // done
+//            .registerSubtype(TareSensor.class, "TareSensor") // done
+//            .registerSubtype(AlertSensor.class, "AlertSensor") // done
+//            .registerSubtype(TanwiarzSensor.class, "TanwiarzSensor") // done
+//            .registerSubtype(PoteznyTanwiarzSensor.class, "PoteznyTanwiarzSensor") // done
+//            .registerSubtype(SettingsSensor.class, "SettingsSensor") // done
+//            .registerSubtype(CompositeBitSensor.class, "CompositeBitSensor") // done
+//            .registerSubtype(ByteSensor.class, "ByteSensor") // done
+//            .registerSubtype(TimerSensor.class, "TimerSensor"); // done
+//
+//    private final RuntimeTypeAdapterFactory<Command> commandAdapterFactory = RuntimeTypeAdapterFactory.of(Command.class, "type")
+//            .registerSubtype(ProtobufCommand.class, "ProtobufCommand") // done
+//            .registerSubtype(ProtobufSimpleCommand.class, "ProtobufSimpleCommand") // done
+//            .registerSubtype(StandardCommand.class, "StandardCommand"); // done
+//
+//    // temp:
+//    // true - zapisuje konfiguracje z do temp
+//    // false - zapisuje konfiguracje do config
+//    public void saveToFile(BaseSaveModel configuration, boolean temp) {
+//        String configContent = new Gson().newBuilder()
+//                .registerTypeAdapterFactory(sensorAdapterFactory)
+//                .registerTypeAdapterFactory(commandAdapterFactory)
+//                .excludeFieldsWithoutExposeAnnotation()
+//                .setPrettyPrinting()
+//                .disableHtmlEscaping()
+//                .create()
+//                .toJson(configuration);
+//
+//        Path configPath = Paths.get(configuration.getPath() + configuration.getFileName());
+//        if (temp) configPath = Paths.get(configuration.getPath(), configuration.getTempFileName());
+//
+//        File configFile = new File(configPath.toString());
+//
+//        try (FileWriter configWriter = new FileWriter(configFile)) {
+//            configWriter.write(configContent);
+//        } catch (Exception e) {
+//            logger.error(e.getMessage());
+//        }
+//    }
+//
+//    public void persistOldFile(BaseSaveModel config) {
+//        File dir = new File(config.getPath());
+//        if(!dir.exists()) {
+//            dir.mkdir();
+//        }
+//
+//        File configFile = new File(config.getPath() + config.getFileName());
+//        File copy = new File(config.getPath()  + config.getPersistPrefix() + config.getFileName());
+//        try (FileInputStream fis = new FileInputStream(configFile);
+//           FileOutputStream fos = new FileOutputStream(copy)) {
+//            int len;
+//            byte[] buffer = new byte[4096];
+//            while ((len = fis.read(buffer)) > 0) {
+//                fos.write(buffer, 0, len);
+//            }
+//        } catch (IOException e) {
+//            logger.error(e.getMessage());
+//        }
+//    }
+//
+//    // temp:
+//    // true — czyta plik z temp
+//    // false — ładuje plik config, a następnie nadpisuje nim temp i czyta plik temp
+//    public <T extends BaseSaveModel> T readFromFile(T config, boolean temp) throws Exception {
+//        if (!temp) {
+//            Path configPath = Paths.get(config.getPath() + config.getFileName());
+//            Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
+//            Files.copy(configPath, configTempPath, StandardCopyOption.REPLACE_EXISTING);
+//        }
+//
+//        File configFile = new File(config.getPath() + config.getTempFileName());
+//
+//        try (JsonReader reader = new JsonReader(new FileReader(configFile))) {
+//            config = new Gson().newBuilder()
+//                    .registerTypeAdapterFactory(sensorAdapterFactory)
+//                    .registerTypeAdapterFactory(commandAdapterFactory)
+//                    .excludeFieldsWithoutExposeAnnotation().create().fromJson(reader, config.getClass());
+//        } catch (Exception e) {
+//            logger.error(e.getMessage());
+//            throw new Exception(e.getMessage());
+//        }
+//
+//        return config;
+//    }
+//
+//    public <T extends BaseSaveModel> void removeTempConfig(T config) {
+//        File configTempFile = new File(config.getPath() + config.getTempFileName());
+//        if (configTempFile.delete()) System.out.println("Removed temp file");
+//        else System.out.println("Temp file not found");
+//    }
+//
+//    public String commandToJson(Command command) {
+//        return new Gson().newBuilder()
+//                .registerTypeAdapterFactory(commandAdapterFactory)
+//                .excludeFieldsWithoutExposeAnnotation()
+//                .setPrettyPrinting()
+//                .disableHtmlEscaping()
+//                .create()
+//                .toJson(command, Command.class);
+//    }
+//
+//    public void addCommandToFile(BaseSaveModel config, Command command) {
+//        String[] commandJson = commandToJson(command).split("\n");
+//
+//        Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
+//
+//        try {
+//            List<String> configLines = Files.readAllLines(configTempPath);
+//            int index = 0;
+//            int squareBracketCounter = 1;
+//            while(!configLines.get(index).contains("commandsList")) index++;
+//            while(squareBracketCounter != 0) {
+//                index++;
+//                if(configLines.get(index).contains("[")) squareBracketCounter++;
+//                if(configLines.get(index).contains("]")) squareBracketCounter--;
+//            }
+//            configLines.set(index-1, configLines.get(index-1) + ",");
+//            for (String line : commandJson) {
+//                configLines.add(index, "\t"+line);
+//                index++;
+//            }
+//
+//            Files.write(configTempPath, configLines);
+//
+//        } catch (FileNotFoundException e) {
+//            logger.error("Config file not found");
+//            logger.error(e.getMessage());
+//        } catch (IOException e) {
+//            logger.error("Error during modifying tempConfig");
+//            logger.error(e.getMessage());
+//        }
+//    }
+//
+//    public void addDestinationToCommand(BaseSaveModel config, Command command, String controllerName) {
+//    }
+//
+//    public void overrideConfig(BaseSaveModel config) {
+//        Path configPath = Paths.get(config.getPath() + config.getFileName());
+//        Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
+//        try {
+//            Files.copy(configTempPath, configPath, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException e) {
+//            logger.error(e.getMessage());
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//}
---
+++ src/main/java/pl/edu/pwr/pwrinspace/poliwrocket/Service/Save/ModelAsYamlService.java
@@ -0,0 +1,156 @@
+package pl.edu.pwr.pwrinspace.poliwrocket.Service.Save;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.NamedType;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.edu.pwr.pwrinspace.poliwrocket.Model.BaseSaveModel;
+import pl.edu.pwr.pwrinspace.poliwrocket.Model.Command.Command;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Set;
+
+public class ModelAsYamlService {
+    private static final Logger logger = LoggerFactory.getLogger(ModelAsYamlService.class);
+    private final ObjectMapper mapper = YAMLMapper.builder().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER).addModule(new ParameterNamesModule()).build();
+
+    public ModelAsYamlService() {
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.CREATOR, JsonAutoDetect.Visibility.ANY);
+
+        Reflections sensorReflections = new Reflections("pl.edu.pwr.pwrinspace.poliwrocket.Model.Sensor");
+        Reflections commandReflections = new Reflections("pl.edu.pwr.pwrinspace.poliwrocket.Model.Command");
+        Set<Class<?>> sensorSubtypes = sensorReflections.getTypesAnnotatedWith(JsonTypeName.class);
+        Set<Class<?>> commandSubtypes = commandReflections.getTypesAnnotatedWith(JsonTypeName.class);
+        for (Class<?> sensorSubtype : sensorSubtypes) {
+            JsonTypeName annotation = sensorSubtype.getAnnotation(JsonTypeName.class);
+            if (annotation != null) {
+                String sensorTypeName = annotation.value();
+                mapper.registerSubtypes(new NamedType(sensorSubtype, sensorTypeName));
+            }
+        }
+        for (Class<?> commandSubtype : commandSubtypes) {
+            JsonTypeName annotation = commandSubtype.getAnnotation(JsonTypeName.class);
+            if (annotation != null) {
+                String commandTypeName = annotation.value();
+                mapper.registerSubtypes(new NamedType(commandSubtype, commandTypeName));
+            }
+        }
+    }
+
+    // temp:
+    // true - zapisuje konfiguracje z do temp
+    // false - zapisuje konfiguracje do config
+    public void saveToFile(BaseSaveModel configuration, boolean temp) {
+        try {
+            String configContent = mapper.writeValueAsString(configuration);
+
+            Path configPath = Paths.get(configuration.getPath() + configuration.getFileName());
+            if (temp) configPath = Paths.get(configuration.getPath(), configuration.getTempFileName());
+
+            File configFile = new File(configPath.toString());
+
+            FileWriter configWriter = new FileWriter(configFile);
+            configWriter.write(configContent);
+            configWriter.close();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void persistOldFile(BaseSaveModel config) {
+        File dir = new File(config.getPath());
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+
+        File configFile = new File(config.getPath() + config.getFileName());
+        File copy = new File(config.getPath()  + config.getPersistPrefix() + config.getFileName());
+        try (FileInputStream fis = new FileInputStream(configFile);
+             FileOutputStream fos = new FileOutputStream(copy)) {
+            int len;
+            byte[] buffer = new byte[4096];
+            while ((len = fis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    // temp:
+    // true — czyta plik z temp
+    // false — ładuje plik config, a następnie nadpisuje nim temp i czyta plik temp
+    public <T extends BaseSaveModel> T readFromFile(T config, boolean temp) throws Exception {
+        if (!temp) {
+            Path configPath = Paths.get(config.getPath() + config.getFileName());
+            Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
+            Files.copy(configPath, configTempPath, StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        File configFile = new File(config.getPath() + config.getTempFileName());
+
+        try {
+            config = (T) mapper.readValue(configFile, config.getClass()); // todo tym trzeba się zająć
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new Exception(e.getMessage());
+        }
+
+        return config;
+    }
+
+    public <T extends BaseSaveModel> void removeTempConfig(T config) {
+        File configTempFile = new File(config.getPath() + config.getTempFileName());
+        if (configTempFile.delete()) System.out.println("Removed temp file");
+        else System.out.println("Temp file not found");
+    }
+
+    public String commandToYaml(Command command) {
+        try {
+            return mapper.writeValueAsString(command);
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage());
+        }
+        return null;
+    }
+
+    public void addCommandToFile(BaseSaveModel config, Command command) {
+        Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
+
+        try {
+            JsonNode configNode = mapper.readTree(new File(configTempPath.toString()));
+            JsonNode commandListNode = configNode.get("commandsList");
+            if (commandListNode != null && commandListNode.isArray()) {
+                ((ArrayNode) commandListNode).add(commandToYaml(command));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void overrideConfig(BaseSaveModel config) {
+        Path configPath = Paths.get(config.getPath() + config.getFileName());
+        Path configTempPath = Paths.get(config.getPath() + config.getTempFileName());
+        try {
+            Files.copy(configTempPath, configPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
+}
